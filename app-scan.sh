47676677b80d28a81ff4ff6958d37ad9a18f98b2
Gunnar Braun <gunnar@synopsys.com>
2022-01-12 19:09:36 +0100

    fixes

--- app-scan.sh
+++ app-scan.sh
@@ -28,21 +28,27 @@ imageIds=( $(docker history --format '{{ .ID }}' --no-trunc $1 | awk '/sha256/ {
 
 # Remove first one as this is the image itself
 unset imageIds[0]
-#echo ${imageIds[@]}
 
+# Search for the last named image (last FROM line in Dockerfile)
 for id in ${imageIds[@]}; do
   tag=$(docker inspect $id | jq ".[].RepoTags[]" | tr -d '"')
   if [ ! -z $tag ]; then
-    if [ ! -z $platform ]; then
-      echo "Error: multiple base images found."
-      exit 1
+    if [ ! -z $baseImageName ]; then
+      # This happens when the base image is not coming from a registry. If an image is pushed
+      # to a registry, the intermediate images are removed. 'docker history' would then show
+      # these as <missing>. However, when the base image is built locally, the intermediate
+      # images (and the base image's base image) are still there.
+      echo "Warning: multiple nested base images found, using latest one."
+      #echo "Using $baseImageName instead of $tag"
+    else
+      baseImageName=$tag
+      baseImageId=$id
     fi
-    baseImageName=$tag
-    baseImageId=$id
   fi
 done
 
+# Find the top layer id of the base image (for passing to Docker Inspector)
 baseImageTop=( $(docker inspect $baseImageId | jq ".[].RootFS.Layers[-1]" | tr -d '"') )
-echo "Base image: $tag"
+echo "Base image: $baseImageName"
 echo "Top layer: ${baseImageTop[-1]}"
 
